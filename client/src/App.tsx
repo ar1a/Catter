1a86a51bcd774ef95667102ac04cc2ce221d1354
Aria Edmonds <aria@ar1as.space>
2019-06-12 12:51:37 +1000

    Set up fonts, material-ui theme, install apollo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import { CssBaseline, Container, createMuiTheme } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
+import purple from '@material-ui/core/colors/purple';
 import './App.css';
+import './jost/jost.css';
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: ['Jost', 'Roboto', 'Helvetica', 'Arial', 'sans-serif'].join(',')
+  },
+  palette: {
+    type: 'dark',
+    primary: purple,
+    secondary: {
+      main: '#f44336'
+    }
+  }
+});
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container>Hello world</Container>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
