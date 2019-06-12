11c66497e5bdfec438e01481813765ef16dcc1e1
Aria Edmonds <aria@ar1as.space>
2019-06-12 13:03:03 +1000

    set up apollo-client

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,7 +4,30 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo-hooks';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  request: operation => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      operation.setContext({
+        headers: {
+          authorization: `Bearer ${token}`
+        }
+      });
+    }
+  }
+});
+
+const WrappedApp = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+ReactDOM.render(<WrappedApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
