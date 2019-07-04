773d6822afb008c2fcd6ab2df7438f0579c331dc
Aria Edmonds <aria@ar1as.space>
2019-07-04 13:29:25 +1000

    likeMeow: check if user is authenticated

--- server/permissions.ts
+++ server/permissions.ts
@@ -22,6 +22,7 @@ export default shield({
   },
   Mutation: {
     postMeow: isAuthenticatedUser,
-    deleteMeow: isAuthenticatedAndOwnsMeow
+    deleteMeow: isAuthenticatedAndOwnsMeow,
+    likeMeow: isAuthenticatedUser
   }
 });
