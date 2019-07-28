4eb8621f8946a98ac5d25f1e59e3e10259011414
Aria Edmonds <aria@ar1as.space>
2019-07-28 23:20:22 +1000

    user: tailwind redesign

--- client/src/user.tsx
+++ client/src/user.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import gql from 'graphql-tag';
 import { useQuery } from 'react-apollo-hooks';
 import { RouteComponentProps } from 'react-router';
-import { Grid, Card, CardContent, Typography } from '@material-ui/core';
-import { makeStyles, createStyles } from '@material-ui/styles';
 import ReactFitText from 'react-fittext';
 import { Link } from 'react-router-dom';
 import ConditionalWrap from 'conditional-wrap';
@@ -45,28 +43,11 @@ const GET_USER = gql`
   }
 `;
 
-const useStyles = makeStyles(
-  createStyles({
-    username: {
-      textAlign: 'center',
-      opacity: 0.8
-    },
-    name: {
-      textAlign: 'center'
-    },
-    link: {
-      textDecoration: 'none',
-      color: 'inherit'
-    }
-  })
-);
-
 export const User: React.FC<RouteComponentProps<{ username: string }>> = ({
   match: {
     params: { username }
   }
 }) => {
-  const classes = useStyles({});
   const myUsername = useUserState('username');
   const { data, error, loading } = useQuery<getuser>(GET_USER, {
     variables: { username },
@@ -88,37 +69,65 @@ export const User: React.FC<RouteComponentProps<{ username: string }>> = ({
     return <div>User not found!</div>; // TODO: go back and show notification
   }
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={12} md={4}>
-        <Card>
-          <CardContent>
-            <ReactFitText>
-              <Typography variant="h3" className={classes.name}>
-                <ConditionalWrap
-                  condition={myUsername === username}
-                  wrap={(children: React.ReactNode) => (
-                    <Link to="/settings/name" className={classes.link}>
-                      {children}
-                    </Link>
-                  )}
-                >
-                  {data.user.name || 'INVALID NAME'}
-                </ConditionalWrap>
-              </Typography>
-            </ReactFitText>
-            <ReactFitText>
-              <Typography variant="h5" className={classes.username}>
-                @{username}
-              </Typography>
-            </ReactFitText>
-          </CardContent>
-        </Card>
-      </Grid>
-      <Grid item xs={12} md={8}>
+    <div className="flex mx-auto p-4 flex-wrap">
+      <div className="w-full md:w-1/3 text-center mb-6">
+        <div>
+          <ReactFitText compressor={2}>
+            <h1>
+              <ConditionalWrap
+                condition={myUsername === username}
+                wrap={(children: React.ReactNode) => (
+                  <Link to="/settings/name">{children}</Link>
+                )}
+              >
+                {data.user.name || 'INVALID NAME'}
+              </ConditionalWrap>
+            </h1>
+          </ReactFitText>
+          <ReactFitText compressor={2.5}>
+            <h2 className="text-gray-700">@{username}</h2>
+          </ReactFitText>
+        </div>
+      </div>
+      <div className="w-full md:w-2/3">
         {(data.user.meows || []).map(meow => (
           <Meow key={meow.id} noUserRedirect meow={meow} />
         ))}
-      </Grid>
-    </Grid>
+      </div>
+    </div>
   );
+  /* return (
+   *   <Grid container spacing={3}>
+   *     <Grid item xs={12} md={4}>
+   *       <Card>
+   *         <CardContent>
+   *           <ReactFitText>
+   *             <Typography variant="h3" className={classes.name}>
+   *               <ConditionalWrap
+   *                 condition={myUsername === username}
+   *                 wrap={(children: React.ReactNode) => (
+   *                   <Link to="/settings/name" className={classes.link}>
+   *                     {children}
+   *                   </Link>
+   *                 )}
+   *               >
+   *                 {data.user.name || 'INVALID NAME'}
+   *               </ConditionalWrap>
+   *             </Typography>
+   *           </ReactFitText>
+   *           <ReactFitText>
+   *             <Typography variant="h5" className={classes.username}>
+   *               @{username}
+   *             </Typography>
+   *           </ReactFitText>
+   *         </CardContent>
+   *       </Card>
+   *     </Grid>
+   *     <Grid item xs={12} md={8}>
+   *       {(data.user.meows || []).map(meow => (
+   *         <Meow key={meow.id} noUserRedirect meow={meow} />
+   *       ))}
+   *     </Grid>
+   *   </Grid>
+   * ); */
 };
